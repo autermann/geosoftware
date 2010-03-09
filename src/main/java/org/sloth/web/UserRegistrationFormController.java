9312658ed549d33555ab7795f5375d3b9a6847e5
Christian Autermann <autermann@uni-muenster.de>
2010-03-09 17:34:23 +0000

    -

--- src/main/java/org/sloth/web/UserRegistrationFormController.java
+++ src/main/java/org/sloth/web/UserRegistrationFormController.java
@@ -2,28 +2,32 @@ package org.sloth.web;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 
 public class UserRegistrationFormController extends SimpleFormController {
 
-    public UserRegistrationFormController() {
-    }
+	public UserRegistrationFormController() {
+	//	this.set
+	}
+/*
+	public ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response, Object command, BindException errors) {
 
-    public ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response, Object command, BindException errors) {
+	UserRegistrationCommand userRegistrationCommand = (UserRegistrationCommand) command;
 
-        UserRegistrationCommand userRegistrationCommand = (UserRegistrationCommand) command;
-
-        return new ModelAndView(getFormView(), errors.getModel());
+	return new ModelAndView(getFormView(), errors.getModel());
 
 
 
-    }
+	}
 
-    public Object formBackingObject(HttpServletRequest request, Object command) {
-        logger.info("Entering into Form Backing Object");
-        UserRegistrationCommand userRegistrationCommand = (UserRegistrationCommand) command;
-        logger.info("Exiting Form Backing Object");
-        return userRegistrationCommand;
+	public Object formBackingObject(HttpServletRequest request, Object command) {
+	logger.info("Entering into Form Backing Object");
+	UserRegistrationCommand userRegistrationCommand = (UserRegistrationCommand) command;
+	logger.info("Exiting Form Backing Object");
+	return userRegistrationCommand;
 
-    }
+	 */
+}
