3a6ac72f94d461c8a8e69848d2ad54ce312e22e2
Christian Autermann <autermann@uni-muenster.de>
2010-03-10 22:59:26 +0000

    -

--- src/main/java/org/sloth/web/AddUserForm.java
+++ src/main/java/org/sloth/web/AddUserForm.java
@@ -20,37 +20,36 @@ import org.springframework.web.bind.support.SessionStatus;
 @SessionAttributes(types = User.class)
 public class AddUserForm {
 
-
 	@Autowired
 	private UserManager userManager;
-	
 	@Autowired
 	private UserValidator userValidator;
-
 
 	public void setUserValidator(UserValidator userValidator) {
 		this.userValidator = userValidator;
 	}
 
-	protected UserValidator getUserValidator(){
+	protected UserValidator getUserValidator() {
 		return this.userValidator;
 	}
-
 
 	public void setUserManager(UserManager userManager) {
 		this.userManager = userManager;
 	}
 
-	protected UserManager getUserManager(){
+	protected UserManager getUserManager() {
 		return this.userManager;
 	}
 
-
 	@InitBinder
 	public void setAllowedFields(WebDataBinder dataBinder) {
-		dataBinder.setDisallowedFields("id","creationDate","userRight");
+		dataBinder.setDisallowedFields("id", "creationDate", "userRight");
 	}
 
+
+	/*
+	 * Beim Aufruf der Seite wird die GET-Methoder aufgerufen...
+	 */
 	@RequestMapping(method = GET)
 	public String setupForm(Model model) {
 		User user = new User();
@@ -58,8 +57,12 @@ public class AddUserForm {
 		return "users/form";
 	}
 
+	/*
+	 * Beim Absenden des Formulars die POST-Methode...
+	 */
 	@RequestMapping(method = POST)
-	public String processSubmit(@ModelAttribute User user, BindingResult result, SessionStatus status) {
+	public String processSubmit(@ModelAttribute User user, BindingResult result,
+								SessionStatus status) {
 		getUserValidator().validate(user, result);
 		if (result.hasErrors()) {
 			return "users/form";
@@ -69,6 +72,5 @@ public class AddUserForm {
 			return "redirect:/";
 		}
 	}
-
 
 }
